5c9e8d33fe0cd1afa77876ae791846e372682d40
Ethan Stark <ethanstark@gmail.com>
2016-09-26 11:41:05 -0700

    started adding api routes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import apiUsersRouter from './api/users'
 
 const server = express()
 
 server.use(express.static(__dirname+'/public'));
+
+server.get('/api/current-user', (request, response) => {
+  response.json({
+    id: 42,
+    email: "me@me.com",
+    password: "123"
+  })
+})
+
+server.use('/api/users', apiUsersRouter)
 
 server.get('/*', (request, response) => {
   response.sendFile(__dirname+'/public/index.html');
