106159f4f2d3db95b5f4e2ccf0fb2f7c1d80cc75
Ethan Stark <ethanstark@gmail.com>
2016-10-06 15:12:09 -0700

    WIP | Ethan, TJ, and Jared on CreateCard

--- browser/components/BoardShowPage.js
+++ browser/components/BoardShowPage.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react'
 import './BoardShowPage.sass'
 import Layout from './Layout'
 import Link from './Link'
+import Icon from './Icon'
+import PresentationalComponent from './PresentationalComponent'
 import $ from 'jquery'
+import CreateCard from './CreateCard'
 
 class BoardProvider extends Component {
   constructor(props){
@@ -56,15 +59,87 @@ const BoardShowPage = ({board}) => {
   </Layout>
 }
 
-const List = ({ list, cards }) => {
-  const cardNodes = cards.map(card => {
-    return <Card key={card.id} card={card} />
-  })
-  return <div className="BoardShowPage-List">
-    <div className="BoardShowPage-ListHeader">{list.name}</div>
-    <div className="BoardShowPage-cards">{cardNodes}</div>
-    <div className="BoardShowPage-add-card">Add a card…</div>
-  </div>
+class List extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      creatingCard: false
+    }
+    this.creatingCard = this.creatingCard.bind(this)
+    this.cancelCreatingCard = this.cancelCreatingCard.bind(this)
+    this.saveCard = this.saveCard.bind(this)
+  }
+
+  creatingCard() {
+    this.setState({creatingCard: true})
+  }
+
+  cancelCreatingCard() {
+    this.setState({creatingCard: false})
+  }
+
+  saveCard(content) {
+    console.log("would save card", content);
+  }
+
+  render(){
+    const { list, cards } = this.props
+    const cardNodes = cards.map(card => {
+      return <Card key={card.id} card={card} />
+    })
+
+    let createCardForm, createCardLink
+    if (this.state.creatingCard) {
+      createCardForm = <CreateCardForm
+        onCancel={this.cancelCreatingCard}
+        onSave={this.saveCard}
+      />
+    } else {
+      createCardLink = <Link onClick={this.creatingCard} >Add a card...</Link>
+    }
+
+    return <div className="BoardShowPage-List">
+      <div className="BoardShowPage-ListHeader">{list.name}</div>
+      <div className="BoardShowPage-cards">
+        {cardNodes}
+        {createCardForm}
+      </div>
+      {createCardLink}
+    </div>
+  }
+}
+
+class CreateCardForm extends Component {
+
+  constructor(props) {
+    super(props)
+    this.onKeyUp = this.onKeyUp.bind(this)
+  }
+
+  componentDidMount() {
+    this.refs.content.focus()
+  }
+
+  onKeyUp(event) {
+    if (event.keyCode === 13) {
+      event.preventDefault()
+      this.createCard()
+    }
+  }
+
+  createCard() {
+    this.props.onSave(this.refs.content.value)
+    this.refs.content.value = ""
+  }
+
+  render() {
+    return <div>
+      <textarea className="BoardShowPage-Card" onKeyUp={this.onKeyUp} ref="content"/>
+      <button onClick={this.createCard}>Add</button>
+      <button onClick={this.props.onCancel}><Icon type="times" /></button>
+    </div>
+  }
 }
 
 const Card = ({ card }) => {
